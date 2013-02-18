ac3f68748ef415e4642ccb24d962985bb83c888d
Justin <justinpinner@gmail.com>
2013-02-18 18:56:57 +0000

    json file loading works

--- main.js
+++ main.js
@@ -64,7 +64,7 @@
 
           // draw a rectangle centered at pt
           var w = 10
-          var color = (node.data.alone) ? "orange" : (node.data.color) ? node.data.color : "black"
+          var color = (node.data.alone) ? "orange" : (node.data.root) ? "blue" : (node.data.color) ? node.data.color : "black"
           ctx.fillStyle = color
           var label = node.data.label || ""          
           if (label){
@@ -132,28 +132,52 @@
     return that
   }    
 
+
+
   $(document).ready(function(){
     var sys = arbor.ParticleSystem(500, 600, 0.5) // create the system with sensible repulsion/stiffness/friction
     sys.parameters({gravity:true}) // use center-gravity to make the graph settle nicely (ymmv)
     sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
 
+      $.getJSON('data/shoehorn-multi.json', function(data) {
+            var output="<ul>";
+            for (var i in data) {
+                sys.addNode(data[i].id, {root: true, label: data[i].headline, links: data[i].links});                
+                output+="<li>" + data[i].id + " " + data[i].headline                
+                for (var l in data[i].links) {
+                    output+= "<li>" + data[i].links[l].id + "</li>"; 
+                }
+                output+= + "</li>";
+            }
+
+            output+="</ul>";
+            
+      });
+
 //    for(x=0; x<json.length; x++){
 //        node = json(x)
-//        sys.addNode(node.id, {length: node.length, label: node.headline, links: node.links});
+//        nodes[x] = sys.addNode(node.id, {length: node.length, label: node.headline, links: node.links});
 //    }
 
-//    foreach(node in 
+//    for(x=0; x<nodes.length; x++){
+//        var links = nodes[x].links;
+//        foreach(link <- links){
+//            linkedNode = nodes.filter(_.id == link.id);
+//            sys.addEdge(nodes[x].id, linkedNode.id);
+//        } 
+//    }
+
+//    alert(nodes.id);
 
     // node a
-    var a = sys.addNode('world/2013/feb/17/oscar-pistorius-cricket-bat', {mass:.50, color:'blue', label:'Oscar Pistorius case: bloodied cricket bat is key evidence, paper claims'});
-    var b = sys.addNode('world/video/2013/feb/18/oscar-pistorius-agent-visits-prison-video', {mass:.50, nodeName: 'foo', color:'green', label:'oscar-pistorius-agent-visits-prison-video'});
-    var c = sys.addNode('sport/2013/feb/17/oscar-pistorius-withdrawn-manchester-city-games', {mass:.50, nodeName: 'foo', color:'green', label:'sport/2013/feb/17/oscar-pistorius-withdrawn-manchester-city-games'});
+//    var a = sys.addNode('world/2013/feb/17/oscar-pistorius-cricket-bat', {mass:.50, color:'blue', label:'Oscar Pistorius case: bloodied cricket bat is key evidence, paper claims'});
+//    var b = sys.addNode('world/video/2013/feb/18/oscar-pistorius-agent-visits-prison-video', {mass:.50, nodeName: 'foo', color:'green', label:'oscar-pistorius-agent-visits-prison-video'});
+//    var c = sys.addNode('sport/2013/feb/17/oscar-pistorius-withdrawn-manchester-city-games', {mass:.50, nodeName: 'foo', color:'green', label:'sport/2013/feb/17/oscar-pistorius-withdrawn-manchester-city-games'});
 
     // add some nodes to the graph and watch it go...
-    sys.addEdge(a, b)
-    sys.addEdge(a, c)
-    sys.addEdge(c, a)
-
+//    sys.addEdge(a, b)
+//    sys.addEdge(a, c)
+//    sys.addEdge(c, a)
 
     //sys.addNode('f', {alone:true, mass:.25})
 
@@ -173,5 +197,6 @@
     // })
     
   })
+
 
 })(this.jQuery)
