26237322ce4cfac197c59a5fa512b52a60e06fd8
Justin <justinpinner@gmail.com>
2013-02-19 11:13:19 +0000

    now with added url

--- main.js
+++ main.js
@@ -11,6 +11,9 @@
     var ctx = canvas.getContext("2d");
     var particleSystem
 
+    ctx.canvas.width  = window.innerWidth - 50;
+    ctx.canvas.height = window.innerHeight - 50;
+
     var that = {
       init:function(system){
         //
@@ -64,11 +67,11 @@
 
           // draw a rectangle centered at pt
           var w = 10
-          var color = (node.data.alone) ? "orange" : (node.data.root) ? "blue" : (node.data.color) ? node.data.color : "black"
+          var color = (node.data.alone) ? "orange" : (node.data.root) ? "red" : (node.data.color) ? node.data.color : "black"
           ctx.fillStyle = color
           var label = node.data.label || ""          
           if (label){
-            ctx.font = "12px Helvetica"
+            ctx.font = "16px Helvetica"
             ctx.fillStyle = color            
             ctx.fillText(label, pt.x-w/2, pt.y-w/2)
           } else {
@@ -132,70 +135,22 @@
     return that
   }    
 
-
-
   $(document).ready(function(){
     var sys = arbor.ParticleSystem(500, 600, 0.5) // create the system with sensible repulsion/stiffness/friction
     sys.parameters({gravity:true}) // use center-gravity to make the graph settle nicely (ymmv)
     sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
 
-      $.getJSON('data/shoehorn-multi.json', function(data) {
-            for (var i in data) {
-                sys.addNode(data[i].id, {root: true, label: data[i].headline, links: data[i].links});                
-                for (var l in data[i].links) {
-                    var node = sys.getNode(data[i].links[l].id);
+      $.getJSON("http://gnm41162:8888/?ignore=type,tone,world/world,technology/technology,uk/uk,culture/culture&callback=?", function(data) {
+            for (var i in data.nodes) {
+                 sys.addNode(data.nodes[i].id, {root:(i == 0), label:data.nodes[i].webTitle, links:data.nodes[i].links});                
+                for (var l in data.nodes[i].links) {
+                    var node = sys.getNode(data.nodes[i].links[l].id);
                     if (node) {
-                        sys.addEdge(data[i].id, node, {length: data[i].links[l].length});
+                        sys.addEdge(data.nodes[i].id, node, {length: data.nodes[i].links[l].length});
                     }                    
                 }
             }
       });
-
-        
-
-//    for(x=0; x<json.length; x++){
-//        node = json(x)
-//        nodes[x] = sys.addNode(node.id, {length: node.length, label: node.headline, links: node.links});
-//    }
-
-//    for(x=0; x<nodes.length; x++){
-//        var links = nodes[x].links;
-//        foreach(link <- links){
-//            linkedNode = nodes.filter(_.id == link.id);
-//            sys.addEdge(nodes[x].id, linkedNode.id);
-//        } 
-//    }
-
-//    alert(nodes.id);
-
-    // node a
-//    var a = sys.addNode('world/2013/feb/17/oscar-pistorius-cricket-bat', {mass:.50, color:'blue', label:'Oscar Pistorius case: bloodied cricket bat is key evidence, paper claims'});
-//    var b = sys.addNode('world/video/2013/feb/18/oscar-pistorius-agent-visits-prison-video', {mass:.50, nodeName: 'foo', color:'green', label:'oscar-pistorius-agent-visits-prison-video'});
-//    var c = sys.addNode('sport/2013/feb/17/oscar-pistorius-withdrawn-manchester-city-games', {mass:.50, nodeName: 'foo', color:'green', label:'sport/2013/feb/17/oscar-pistorius-withdrawn-manchester-city-games'});
-
-    // add some nodes to the graph and watch it go...
-//    sys.addEdge(a, b)
-//    sys.addEdge(a, c)
-//    sys.addEdge(c, a)
-
-    //sys.addNode('f', {alone:true, mass:.25})
-
-    // or, equivalently:
-    //
-    // sys.graft({
-    //   nodes:{
-    //     f:{alone:true, mass:.25}
-    //   }, 
-    //   edges:{
-    //     a:{ b:{},
-    //         c:{},
-    //         d:{},
-    //         e:{}
-    //     }
-    //   }
-    // })
-    
   })
-
 
 })(this.jQuery)
